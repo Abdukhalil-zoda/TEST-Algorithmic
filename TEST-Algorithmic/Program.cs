66d9cbf578cfd65df06ea1092a14f6a24d9684de
Abdulaziz <40052862+Abdukhalil-zoda@users.noreply.github.com>
2021-11-18 11:47:23 +0500

    Добавьте файлы проекта.

---
+++ TEST-Algorithmic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+Console.WriteLine("Enter url[Enter = Default]:");
+var url = Console.ReadLine();
+if (string.IsNullOrEmpty(url))
+{
+    url = "https://algorithmic-dk.herokuapp.com/api/problems/get/1";
+    Console.WriteLine($"[Default]{url}");
+}
+int counter = 0;
+while (true)
+{
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    counter++;
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+    Stream resStream = response.GetResponseStream();
+    watch.Stop();
+    var elapsedMs = watch.ElapsedMilliseconds;
+
+    Console.WriteLine($"[{counter}\t]Status:{response.StatusCode} Time:{elapsedMs}ms");
+}
